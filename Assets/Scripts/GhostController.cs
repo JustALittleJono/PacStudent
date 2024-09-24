4c20e9ec58a74e05375ee35496e380313ef7b08b
JustALittleJono <Jonathanghillier@gmail.com>
2024-09-25 02:30:45 +1000

    trying to animate!

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private Animator animator;
+    [SerializeField]private Animator animator2;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        HandleMovement();
+    }
+    
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetInteger("Facing", 0); // Left
+            animator2.SetInteger("Facing", 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetInteger("Facing", 1); // Down
+            animator2.SetInteger("Facing", 1);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetInteger("Facing", 2); // Right
+            animator2.SetInteger("Facing", 2);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetInteger("Facing", 3); // Up
+            animator2.SetInteger("Facing", 3);
+        }
+    }
+
+    void facing()
     {
         
     }
