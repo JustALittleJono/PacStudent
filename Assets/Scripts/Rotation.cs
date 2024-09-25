4bc0e2bdfa4ab64ed37e6d42fad01aca75b2562c
JustALittleJono <Jonathanghillier@gmail.com>
2024-09-25 18:27:52 +1000

    More animations finished

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    //[SerializeField]private GameObject pacMonster;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,37 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        detectInput();
+    }
+
+    void detectInput()
+    {
         
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.identity;
+            if (transform.localScale.x < 0)
+            {
+                transform.localScale =
+                    new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0,0,90);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.identity;
+            if (transform.localScale.x > 0)
+            {
+                transform.localScale =
+                    new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0,0,-90);
+        }
     }
 }
