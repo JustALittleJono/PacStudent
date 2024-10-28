513fb300f01b28c01c67b3a826fbabbc02ead235
JustALittleJono <Jonathanghillier@gmail.com>
2024-10-28 16:49:53 +1100

    Clean Up

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -11,47 +11,47 @@ public class GhostController : MonoBehaviour
     {
         
     }
-    void CycleAnimations()
-    {
-        // Update the animation direction
-        animator.SetInteger("Facing", currentDirection);
-        animator2.SetInteger("Facing", currentDirection);
-
-        // Increment the direction value and loop back to 0 if it exceeds 3
-        currentDirection = (currentDirection + 1) % 4;
-    }
+    // void CycleAnimations()
+    // {
+    //     // Update the animation direction
+    //     animator.SetInteger("Facing", currentDirection);
+    //     animator2.SetInteger("Facing", currentDirection);
+    //
+    //     // Increment the direction value and loop back to 0 if it exceeds 3
+    //     currentDirection = (currentDirection + 1) % 4;
+    // }
     
     //control ghost eyes with arrow keys
-    // void Update()
-    // {
-    //     HandleMovement();
-    // }
-    //
-    // void HandleMovement()
-    // {
-    //     //left
-    //     if (Input.GetKey(KeyCode.LeftArrow))
-    //     {
-    //         animator.SetInteger("Facing", 0);
-    //         animator2.SetInteger("Facing", 0);
-    //     }
-    //     // Down
-    //     else if (Input.GetKey(KeyCode.DownArrow))
-    //     {
-    //         animator.SetInteger("Facing", 1); 
-    //         animator2.SetInteger("Facing", 1);
-    //     }
-    //     // Right
-    //     else if (Input.GetKey(KeyCode.RightArrow))
-    //     {
-    //         animator.SetInteger("Facing", 2); 
-    //         animator2.SetInteger("Facing", 2);
-    //     }
-    //     // Up
-    //     else if (Input.GetKey(KeyCode.UpArrow))
-    //     {
-    //         animator.SetInteger("Facing", 3); 
-    //         animator2.SetInteger("Facing", 3);
-    //     }
-    // }
+     void Update()
+     {
+         HandleMovement();
+     }
+    
+     void HandleMovement()
+     {
+         //left
+         if (Input.GetKey(KeyCode.LeftArrow))
+         {
+             animator.SetInteger("Facing", 0);
+             animator2.SetInteger("Facing", 0);
+         }
+         // Down
+         else if (Input.GetKey(KeyCode.DownArrow))
+         {
+             animator.SetInteger("Facing", 1); 
+             animator2.SetInteger("Facing", 1);
+         }
+         // Right
+         else if (Input.GetKey(KeyCode.RightArrow))
+         {
+             animator.SetInteger("Facing", 2); 
+             animator2.SetInteger("Facing", 2);
+         }
+         // Up
+         else if (Input.GetKey(KeyCode.UpArrow))
+         {
+             animator.SetInteger("Facing", 3); 
+             animator2.SetInteger("Facing", 3);
+         }
+     }
 }
